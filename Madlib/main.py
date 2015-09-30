a96facc76da3142a62f22bbe75d44489ffd4092e
allison sharpe <allisonsharpe@ymail.com>
2015-09-30 13:26:19 -0400

    Madlib
    Work in progress

--- Madlib/main.py
+++ Madlib/main.py
@@ -2,12 +2,20 @@
 #Date: 09/28/2015
 #Assignment: Madlib
 
+#My story line will be based off of the game, Far cry 3 
+print "Welcome, my fellow American!" 
 
-def madlib():
-	print "Welcome, user!" 
-	
+#Variables 	
 name = raw_input("Enter your first and last name") 
-#add more variables such as "date", "favorite color", etc..
+dob = raw_input("Enter your date of birth");
+lob = raw_input("Where were you born?"); 
+location = raw_input("Where are you located now?");
 
-#story line goes here
-print "Hello there, " ,name
+
+#Function
+ #def money(many_fruit,fruit_cost):
+        #total = many_fruit * fruit_cost
+        #return total
+
+#The journey starts here 
+print "Welcome to " + location + ",", name + ". " + "Upon doing research, it states here that you were born in " + lob + "," + dob + ". "
