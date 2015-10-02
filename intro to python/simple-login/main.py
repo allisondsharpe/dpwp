57223aecc1e01624995887cc2e37ba81e7429a71
allison sharpe <allisonsharpe@ymail.com>
2015-10-02 16:47:46 -0400

    Google App Engine
    Practice

--- intro to python/simple-login/main.py
+++ intro to python/simple-login/main.py
@@ -1,25 +1,23 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+'''
+Name: Allison Sharpe
+Date: 10/2/15
+Class:
+Assignment: 
+'''
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
+import webapp2 #use webapp2 library
+
+class MainHandler(webapp2.RequestHandler): #declaring a class
+    def get(self): #function that starts everything. Catalyst - starts reaction
         self.response.write('Hello world!')
+        #code goes here
+        
+    def additional_functions(self):
+      	pass
+     	#code goes here   
 
+
+#makes python work in browser, do not touch
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
