a8c98d3e1f8a90167463ba4cd18a857be7a9d4aa
allison sharpe <allisonsharpe@ymail.com>
2015-10-01 23:35:24 -0400

    Madlib
    Final Commit

--- Madlib/copy.py
+++
@@ -1,8 +0,0 @@
-age = raw_input("Enter your age:") #enters age
-name = raw_input("Enter your name:") #enters name of user
-
-#Conditional Statement 1  
-if age > "18" and age < "80": #logical operator
-	print name + " will go to the store without guidance." 
-else:
-	print name + " will need a parent/care-taker in order to go to the store."	
