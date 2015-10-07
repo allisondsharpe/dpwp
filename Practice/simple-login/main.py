1fa6e2455030dae949e5227a75f2834ab98c7f8d
allison sharpe <allisonsharpe@ymail.com>
2015-10-07 17:06:37 -0400

    Simple Form Part 1
    Practice

--- Practice/simple-login/main.py
+++ Practice/simple-login/main.py
@@ -1,20 +1,32 @@
 '''
 Name: Allison Sharpe
 Date: 10/2/15
-Class:
-Assignment: 
+Class: Design Patters for Web Programming
+Assignment: Simple Form
 '''
 
 import webapp2 #use webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self): #function that starts everything. Catalyst - starts reaction
-        self.response.write('Hello world!')
-        #code goes here
-        
-    def additional_functions(self):
-      	pass
-     	#code goes here   
+        page = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> Simple Form </title>
+    </head>
+    <body>
+        <form method="GET">
+            <label>Name:</label><input type="text" name="user" />
+            <label>Email:</label><input type="text" name="email" />
+            <input type="submit" value="Submit" />
+        </form>
+    </body>
+</html>'''
+        if self.request.GET: #same as == True
+            #stores info we got from the form
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+        self.response.write(page) #printing out information
 
 
 #makes python work in browser, do not touch
