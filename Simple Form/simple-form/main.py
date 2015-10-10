0db8132ed003cc32e0192bea2a0b42732f6cd3f4
allison sharpe <allisonsharpe@ymail.com>
2015-10-09 21:45:23 -0400

    Simple Form
    Revise

--- Simple Form/simple-form/main.py
+++ Simple Form/simple-form/main.py
@@ -1,27 +1,25 @@
+'''
+Name: Allison Sharpe
+Date: 10/7/15
+Class: Design Patters for Web Programming
+Assignment: Simple Form
+'''
 
-import webapp2   # what handles going to a browser
-from page import Page
+import webapp2 
+from page import Page #imports class Page()
 
-#master class
-class MainHandler(webapp2.RequestHandler):
-
-    #unique ti framework...
-    #THIS FUNCTION RUNS FIRST!! CATALYST
-    def get(self):
-
-        #start writing the code
-
+class MainHandler(webapp2.RequestHandler): #MainHandler class 
+    def get(self):    
 
         if self.request.GET:
-            info = self.request.GET["first_name"] + ' ' + self.request.GET["last_name"]
-
-            page = Page(self)  # creates a page object
-            self.response.write(page.print_contents(info))
+        	#retrieves information from Page to be printed to browser 
+            data = self.request.GET["name"] + ' ' + self.request.GET["email"] + ' ' + self.request.GET["address"] + ' ' + self.request.GET["phone"] + ' ' + self.request.GET["relocate"] + ' ' + self.request.GET["dept"] 
+            page = Page(self)  #retrieves page information
+            self.response.write(page.print_info(data)) #prints information to browser
         else:
-            page = Page(self)  # creates a page object
-            self.response.write(page.print_contents())
-
-#associates class with framework
+            page = Page(self) #retrieves page information
+            self.response.write(page.print_info()) #prints information to browser
+            
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
--- Simple Form/simple-form/page.py
+++ Simple Form/simple-form/page.py
@@ -1,49 +1,49 @@
-class Page():
+'''
+Name: Allison Sharpe
+Date: 10/7/15
+Class: Design Patters for Web Programming
+Assignment: Simple Form
+'''
 
-    def __init__(self, main):
-        self.head = '''<!DOCTYPE HTML>
+class Page(): #class for Page()
+    def __init__(self, page_main): #instances set for class Page()
+        self.css = "css/style.css" #css stylesheet
+        self.head = '''<!DOCTYPE HTML> <!-- inserts html -->
         <html>
-            <head>
-                <title>Simple Form</title>
-                <link href="style.css" type="text/css" rel="stylesheet" />
+            <head> <!-- header tag -->
+                <title>Sharpe's Inc. - Apply Today</title> <!-- Sharpe's Inc. is a made up company -->
+       			<link href="{self.css}" rel="stylesheet" type="text/css" /> <!-- link to my stylesheet -->
             </head>
             <body>
             '''
-            self.body = '''<form method="GET">
-            <h1> Sharpe's Inc. Application Form </h1> <!-- header -->
-            <p> Fill out the form below to get started. </p>
-            <label>Name:</label><input type="text" name="name" class="fields"/><br/><br/> <!-- collects name of user -->
-            <label>Email:</label><input type="text" name="email" class="fields"/><br/><br/> <!-- collects email of user -->
-            <label>Address:</label><input type="text" name="address" class="fields"/><br/><br/> <!-- collects address of user -->
-            <label>Phone:</label><input type="text" name="phone" class="fields" /><br/><br/> <!-- collects phone number of user -->
-            <label>Job Dept: <!-- select options for job department -->
-                <select name="dept">
-                    <option value="Headquarters">Headquarters</option>
-                    <option value="Distribution">Distribution Centers</option>
-                </select><br/><br/>
-            </label>
-            <label>Would you be willing to relocate if required? <!-- radio option for relocation -->
-                <input type="radio" name="relocate" value="Yes" checked> Yes
-                <input type="radio" name="relocate" value="No" checked> No <br/><br/>
-            </label>
-            <a href="../new-page.py"><input type="submit" id="submit" value="Submit"></a> <!-- submit button for form -->
-        '''
-
+        self.body = '''
+        		<form method="GET">
+            		<h1> Sharpe's Inc. Application Form </h1> <!-- main header -->
+            		<p> Fill out the form below to get started. </p>
+            		<label>Name:</label><input type="text" name="name" class="fields"/><br/><br/> <!-- collects name of user -->
+            		<label>Email:</label><input type="text" name="email" class="fields"/><br/><br/> <!-- collects email of user -->
+            		<label>Address:</label><input type="text" name="address" class="fields"/><br/><br/> <!-- collects address of user -->
+            		<label>Phone:</label><input type="text" name="phone" class="fields" /><br/><br/> <!-- collects phone number of user -->
+            		<label>Job Dept: <!-- select options for job department -->
+                	<select name="dept">
+                    	<option value="Headquarters">Headquarters</option>
+                    	<option value="Distribution">Distribution Centers</option>
+                	</select><br/><br/>
+           	 		</label>
+            		<label>Would you be willing to relocate if required? <!-- radio option for relocation -->
+                		<input type="radio" name="relocate" value="Yes" checked> Yes
+                		<input type="radio" name="relocate" value="No" checked> No <br/><br/>
+            		</label>
+            		<a href="../new-page.py"><input type="submit" id="submit" value="Submit"></a> <!-- submit button for form -->
+            	</form>	'''
         self.close = '''
             </body>
         </html>
-            '''
+        	'''
 
-        #main_self is SCOPED to __init__
-        main.response.write("blablabablal")
-
-    def print_contents(self, i=''):
+    def print_info(self, i=''):
         if i=='':
-            return self.head + self.body + self.close + self.ender
+            return self.head + self.body + self.close
         else:
             return self.head + i + self.close
 
-class Button():
-    def __init__(self):
-        self.label = "Contact"
-        self.size = 100
