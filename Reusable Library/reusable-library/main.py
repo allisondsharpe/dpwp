c68d0062cbbe533ccf2bf746546367df8d431728
allison sharpe <allisonsharpe@ymail.com>
2015-10-11 19:56:46 -0400

    Reusable Library
    Work in Progress

---
+++ Reusable Library/reusable-library/main.py
@@ -0,0 +1,38 @@
+import webapp2
+from library import MovieData, FavoriteMovies
+from pages import Page
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+
+        #page for class
+        p = Page()
+        lib = FavoriteMovies()
+
+        #movie title
+        #year movie was made
+        #director of the file
+        md1 = MovieData()
+        md1.title = "The Princess Bride"
+        md1.year = 1989 #actually calling a function
+        md1.director = "Rob Reiner"
+        lib.add_movie(md1)
+
+        md2 = MovieData()
+        md2.title = "Dune"
+        md2.year = 1986 #actually calling a function
+        md2.director = "David Lynch"
+        lib.add_movie(md2)
+
+        md2 = MovieData()
+        md2.title = "Star Wars"
+        md2.year = 1977 #actually calling a function
+        md2.director = "George Lucas"
+        lib.add_movie(md2)
+
+        p.body = lib.compile_list() + lib.calc_time_span()
+        self.response.write(p.print_out())
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
