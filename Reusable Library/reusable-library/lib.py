3b322697ec130a82912375f373313c7a1a5fdc46
allison sharpe <allisonsharpe@ymail.com>
2015-10-13 23:09:41 -0400

    Reusable Library
    Work in Progress

--- Reusable Library/reusable-library/lib.py
+++ Reusable Library/reusable-library/lib.py
@@ -8,58 +8,45 @@ Assignment: Reusable Library
 
 class Students(object): #Class for Students
     def __init__(self):
-        self.__score1 = 0
-        self.__score2 = 0
-        self.__score3 = 0
-        self.__score4 = 0
-        self.__final_score = 0
+        self.__first = ''
+        self.__last = ''
+        self.__level = 0
+        self.__gpa = 0
 
     #Getters
     @property
-    def score_1(self):
-        return self.__score1
+    def first_name(self):
+        return self.__first
 
     @property
-    def score_2(self):
-        return self.__score2
+    def last_name(self):
+        return self.__last
 
     @property
-    def score_3(self):
-        return self.__score3
+    def grade_level(self):
+        return self.__level
 
     @property
-    def score_4(self):
-        return self.__score4
+    def gpa(self):
+        return self.__gpa
 
-    @property
-    def final_score(self):
-        return self.__final_score
 
     #Setters
-    @score_1.setter
-    def score_1(self, new_final_score):
-        self.__score1 = new_final_score
+    @first_name.setter
+    def first_name(self, rp_first):
+        self.__first = rp_first
 
-    @score_2.setter
-    def score_2(self, new_final_score):
-       self.__score2 = new_final_score
+    @last_name.setter
+    def last_name(self, rp_last):
+       self.__score2 = rp_last
 
-    @score_3.setter
-    def score_3(self, new_final_score):
-        self.__score3 = new_final_score
+    @grade_level.setter
+    def grade_level(self, rp_level):
+        self.__level = rp_level
 
-    @score_4.setter
-    def score_4(self, new_final_score):
-       self.__score4 = new_final_score
-
-    @final_score.setter
-    def final_score(self, new_final_score):
-        self.__final_score = new_final_score
-
-    def calc_score(self):
-        #Calculate final grade
-        self.__final_score = (self.__score1 + self.__score2 + self.__score3 + self.__score4 + self.final_score)/5
-
+    @gpa.setter
+    def gpa(self, rp_gpa):
+       self.__score4 = rp_gpa
 
 
 class StudentData(object): #Class for StudentData
