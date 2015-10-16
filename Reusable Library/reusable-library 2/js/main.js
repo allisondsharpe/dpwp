b383bc5b4f1dd382d86a5dfd25c9375bd0a9544e
allison sharpe <allisonsharpe@ymail.com>
2015-10-15 23:51:46 -0400

    Reusable Library
    Final

---
+++ Reusable Library/reusable-library 2/js/main.js
@@ -0,0 +1,192 @@
+function validateForm() { //Function for validateForm - Used to validate each user input field inside the form
+
+    //COMPETITOR #1
+    var x = document.forms["userForm"]["s1_name"].value; //Validates the "s1_name" field
+    if (x == null || x == "") {
+        alert("Please fill out the name field for competitor 1");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s1_score1"].value; //Validates the "s1_score1" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 1 for competitor 1");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s1_score2"].value; //Validates the "s1_score2" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 2 for competitor 1");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s1_score3"].value; //Validates the "s1_score3" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 3 for competitor 1");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s1_score4"].value; //Validates the "s1_score4" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 4 for competitor 1");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s1_score5"].value; //Validates the "s1_score5" field
+    if (x == null || x == "") {
+        alert("Please fill out the final score field for competitor 1");
+        return false;
+    }
+
+    //COMPETITOR #2
+
+    var x = document.forms["userForm"]["s2_name"].value; //Validates the "s2_name" field
+    if (x == null || x == "") {
+        alert("Please fill out the name field for competitor 2");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s2_score1"].value; //Validates the "s2_score1" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 1 for competitor 2");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s2_score2"].value; //Validates the "s2_score2" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 2 for competitor 2");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s2_score3"].value; //Validates the "s2_score3" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 3 for competitor 2");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s2_score4"].value; //Validates the "s2_score4" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 4 for competitor 2");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s2_score5"].value; //Validates the "s2_score5" field
+    if (x == null || x == "") {
+        alert("Please fill out the final score field for competitor 2");
+        return false;
+    }
+
+    //COMPETITOR #3
+
+    var x = document.forms["userForm"]["s3_name"].value; //Validates the "s3_name" field
+    if (x == null || x == "") {
+        alert("Please fill out the name field for competitor 3");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s3_score1"].value; //Validates the "s3_score1" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 1 for competitor 3");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s3_score2"].value; //Validates the "s3_score2" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 2 for competitor 2");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s3_score3"].value; //Validates the "s3_score3" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 3 for competitor 3");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s3_score4"].value; //Validates the "s3_score4" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 4 for competitor 3");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s3_score5"].value; //Validates the "s3_score5" field
+    if (x == null || x == "") {
+        alert("Please fill out the final score field for competitor 3");
+        return false;
+    }
+
+    //COMPETITOR #4
+
+    var x = document.forms["userForm"]["s4_name"].value; //Validates the "s4_name" field
+    if (x == null || x == "") {
+        alert("Please fill out the name field for competitor 4");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s4_score1"].value; //Validates the "s4_score1" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 1 for competitor 4");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s4_score2"].value; //Validates the "s4_score2" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 2 for competitor 4");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s4_score3"].value; //Validates the "s4_score3" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 3 for competitor 4");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s4_score4"].value; //Validates the "s4_score4" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 4 for competitor 4");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s4_score5"].value; //Validates the "s4_score5" field
+    if (x == null || x == "") {
+        alert("Please fill out the final score field for competitor 4");
+        return false;
+    }
+
+    //COMPETITOR #5
+
+    var x = document.forms["userForm"]["s5_name"].value; //Validates the "s1_name" field
+    if (x == null || x == "") {
+        alert("Please fill out the name field for competitor 5");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s5_score1"].value; //Validates the "s1_score1" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 1 for competitor 5");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s5_score2"].value; //Validates the "s5_score2" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 2 for competitor 5");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s5_score3"].value; //Validates the "s5_score3" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 3 for competitor 5");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s5_score4"].value; //Validates the "s5_score4" field
+    if (x == null || x == "") {
+        alert("Please fill out the field for score 4 for competitor 5");
+        return false;
+    }
+
+    var x = document.forms["userForm"]["s5_score5"].value; //Validates the "s5_score5" field
+    if (x == null || x == "") {
+        alert("Please fill out the final score field for competitor 5");
+        return false;
+    }
+
+}
---
+++ Reusable Library/reusable-library 2/main.py
@@ -0,0 +1,73 @@
+'''
+Name: Allison Sharpe
+Date: 10-12-15
+Class: Design Patterns for Web Programming
+Assignment: Reusable Library
+'''
+
+
+import webapp2 #Imports webapp for MainHandler() class
+from library import Scores, ScoreData #Importing class Scores() and class (ScoreData)
+from pages import ResultsPage, FormPage #Importing class ResultsPage() and class FormPage()
+
+class MainHandler(webapp2.RequestHandler): #MainHandler() class
+    def get(self):
+        s = ScoreData() #Instance created for ScoreData() class
+
+        if self.request.GET:
+
+            s1 = ScoreData() #Instance for s1 created within ScoreData()
+            s1.name = self.request.GET['s1_name'] #
+            s1.score1 = (self.request.GET['s1_score1'])
+            s1.score2 = (self.request.GET['s1_score2'])
+            s1.score3 = (self.request.GET['s1_score3'])
+            s1.score4 = (self.request.GET['s1_score4'])
+            s1.final_score = (self.request.GET['s1_final'])
+            s.add_new_competitor(s1) #Adds information for s5 to the ScoreData() class
+
+            s2 = ScoreData() #Instance for s2 created within ScoreData()
+            s2.name = self.request.GET['s2_name']
+            s2.score1 = (self.request.GET['s2_score1'])
+            s2.score2 = (self.request.GET['s2_score2'])
+            s2.score3 = (self.request.GET['s2_score3'])
+            s2.score4 = (self.request.GET['s2_score4'])
+            s2.final_score = (self.request.GET['s2_final'])
+            s.add_new_competitor(s2) #Adds information for s2 to the ScoreData() class
+
+            s3 = ScoreData() #Instance for s3 created within ScoreData()
+            s3.name = self.request.GET['s3_name']
+            s3.score1 = (self.request.GET['s3_score1'])
+            s3.score2 = (self.request.GET['s3_score2'])
+            s3.score3 = (self.request.GET['s3_score3'])
+            s3.score4 = (self.request.GET['s3_score4'])
+            s3.final_score = (self.request.GET['s3_final'])
+            s.add_new_competitor(s3) #Adds information for s3 to the ScoreData() class
+
+            s4 = ScoreData() #Instance for s4 created within ScoreData()
+            s4.name = self.request.GET['s4_name']
+            s4.score1 = (self.request.GET['s4_score1'])
+            s4.score2 = (self.request.GET['s4_score2'])
+            s4.score3 = (self.request.GET['s4_score3'])
+            s4.score4 = (self.request.GET['s4_score4'])
+            s4.final_score = (self.request.GET['s4_final'])
+            s.add_new_competitor(s4) #Adds information for s4 to the ScoreData() class
+
+            s5 = ScoreData() #Instance for s5 created within ScoreData()
+            s5.name = self.request.GET['s5_name']
+            s5.score1 = (self.request.GET['s5_score1'])
+            s5.score2 = (self.request.GET['s5_score2'])
+            s5.score3 = (self.request.GET['s5_score3'])
+            s5.score4 = (self.request.GET['s5_score4'])
+            s5.final_score = (self.request.GET['s5_final'])
+            s.add_new_competitor(s5) #Adds information for s5 to the ScoreData() class
+
+            rp = ResultsPage() #Instance created for ResultsPage() class - Collects resulting information
+            rp.body = (s.calc_score1() + s.calc_score2() + s.calc_score3() + s.calc_score4() + s.calc_final()) #Gathering the results in the body from the ResultsPage() class
+            self.response.write(rp.print_out()) #Prints the ResultsPage() class to the browser
+        else:
+            fp = FormPage() #Instance created for FormPage() class - Collects user input
+            self.response.write(fp.print_out()) #Prints the FormPage() class to the browser
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
