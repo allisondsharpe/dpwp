e3656484216aba98103dd5e14377761d171ca781
allison sharpe <allisonsharpe@ymail.com>
2015-10-16 17:25:59 -0400

    Inheritance
    Practice

--- Practice/inheritance/main.py
+++ Practice/inheritance/main.py
@@ -3,8 +3,9 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        p = Page()
-        self.response.write(p.print_out())
+        p = FormPage()
+        p.inputs = [['first_name', 'text','First Name'], ['last_name', 'text','Last Name'], ['Submit', 'submit']]
+        self.response.write(p.print_out_form())
 
 class Page(object): #borrowing stuff from the object class
     def __init__(self):
@@ -16,7 +17,7 @@ class Page(object): #borrowing stuff from the object class
     </head>
     <body> '''
 
-        self._body = ''
+        self._body = 'Filler'
         self._close = '''
     </body>
 </html> '''
@@ -30,9 +31,35 @@ class FormPage(Page): #sub class for Page - Page is super page/parent class to F
         super(FormPage, self).__init__() #Page.__init__()
         self._form_open = '<form method="GET">'
         self._form_close = '</form>'
+        self.__inputs = []
+        self._form_inputs = ''
         #<input type="text" value="" name="first_name" placeholder="First Name" />
+        #['first_name'<-- placeholder, text,<-- text 'First Name'<-- value]
         #<input type="text" value="" name="last_name" placeholder="Last Name" />
         #<input type="submit" value="Submit" />
+
+    @property
+    def inputs(self):
+       pass
+
+    @inputs.setter
+    def inputs(self, arr):
+        #change my private inputs variable
+        self.__inputs = arr
+        #sort through the mega array and create HTML inputs based on the info there
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+            #if there is a third item.. add it in..
+            try:
+                self._form_inputs += '" placeholder="' + item[2] + '" />'
+            #otherwise.. end the tag
+            except:
+                self._form_inputs += '" />'
+
+            print self._form_inputs
+
+    def print_out_form(self):
+       return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
